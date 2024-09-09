1fc5b318f3c209bed116f76208dba803f3ef6b9b
vinihsilv <vinihsilv657@gmail.com>
2024-09-09 17:29:37 -0300

    inicializando projeto

---
+++ src/test/java/com/vinivhs/demo_park_api/DemoParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinivhs.demo_park_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
