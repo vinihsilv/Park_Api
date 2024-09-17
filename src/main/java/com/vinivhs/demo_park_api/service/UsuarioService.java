9c930189a3aa4b1a011a18538553da6395fa1788
vinihsilv <vinihsilv657@gmail.com>
2024-09-17 18:58:08 -0300

    criando recurso para criar um novo usuário

--- src/main/java/com/vinivhs/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/vinivhs/demo_park_api/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.vinivhs.demo_park_api.service;
 
+import com.vinivhs.demo_park_api.entity.Usuario;
 import com.vinivhs.demo_park_api.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,10 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+
+        return usuarioRepository.save(usuario);
+    }
 }
--- src/main/java/com/vinivhs/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/vinivhs/demo_park_api/web/controller/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.vinivhs.demo_park_api.web.controller;
 
+import com.vinivhs.demo_park_api.entity.Usuario;
 import com.vinivhs.demo_park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,11 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.salvar(usuario);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 
 }
