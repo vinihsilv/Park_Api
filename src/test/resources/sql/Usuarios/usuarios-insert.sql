7e6b263712dc142fd8245a2d9f29a5de8bd1fdc7
vinihsilv <vinihsilv657@gmail.com>
2024-10-02 20:33:34 -0300

    adicionando implementação do banco H2 e notações para teste end-to-end

---
+++ src/test/resources/sql/Usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS (id, username, password, role) values(100, 'ana@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values(101, 'vini@email.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values(102, 'milena@email.com', '123456', 'ROLE_CLIENTE');
