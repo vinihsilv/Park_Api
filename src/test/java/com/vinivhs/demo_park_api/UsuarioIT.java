e9bc161281b26dc64547b584038602f9ea435e66
vinihsilv <vinihsilv657@gmail.com>
2024-10-25 16:58:18 -0300

    adicionando teste para localizar usuario por id

--- src/test/java/com/vinivhs/demo_park_api/UsuarioIT.java
+++ src/test/java/com/vinivhs/demo_park_api/UsuarioIT.java
@@ -123,4 +123,31 @@ public class UsuarioIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
 
     }
+    @Test
+    public void buscarUsuario_ComIdExistente_RetornarUsuarioComStatus200(){
+        UsuarioResponseDto responseBody = testClient
+                .get()
+                .uri("/api/v1/usuarios/100")
+                .exchange().expectStatus().isOk().expectBody(UsuarioResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isEqualTo(100);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("ana@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("ADMIN");
+
+    }
+    @Test
+    public void buscarUsuario_ComIdInexistente_RetornarErrorMessageComStatus404(){
+        ErrorMessage responseBody = testClient
+                .get()
+                .uri("/api/v1/usuarios/0")
+                .exchange().expectStatus().isNotFound().expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(404);
+
+
+    }
 }
